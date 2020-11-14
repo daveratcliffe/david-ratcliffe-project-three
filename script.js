8460378a60b50e52f844923c0ae670055f16682e
David <daveratcliffe93@gmail.com>
2020-11-14 14:52:02 -0500

    finished js code for number select and restart

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Choosies MVP
 const myApp = {};
+// number option callback function
 myApp.handleChoiceClick = function () {
   // save selected number from user
   const userNumber = parseInt($(this).val());
@@ -7,25 +7,38 @@ myApp.handleChoiceClick = function () {
   $('.numberOption').attr('disabled', true);
   // save randomly generate number between zero and five for computer player
   const opponentNumber = Math.floor(Math.random() * 5) + 1;
+  console.log(opponentNumber);
   // have the chosen number for the user and the computer display on the page with a hand
   const displayHand = function () {
     $(`.evenFinger${userNumber}`).css('z-index', '10');
     $(`.finger${opponentNumber}`).css('z-index', '10');
   };
   displayHand();
-  // compare if even or odd wins by using modulo
+  // set score counter to zero
+  // compare if even or odd wins by using modulos
   // display "winner"/"lose" on the page if the user wins/loses
   const summedFingers = userNumber + opponentNumber;
   if (summedFingers % 2 === 0) {
     $('.winSum').html(`${summedFingers} fingers`);
     $('.winDeclaration').html('evens wins');
     $(`.playAgain`).css('visibility', 'visible');
+    // add score to user
+    let userScore = 0;
+    userScore += 1;
+    $('.evenScore').html(userScore);
+    console.log(userScore);
   } else {
     $('.winSum').html(`${summedFingers} fingers`);
     $('.winDeclaration').html('odds wins');
     $(`.playAgain`).css('visibility', 'visible');
+    // add score to opponent
+    let opponentScore = 0;
+    opponentScore += 1;
+    $('.oddScore').html(opponentScore);
+    console.log(opponentScore);
   }
 };
+
 // create a play again button the resets the code
 myApp.handlePlayAgainClick = function () {
   $('.handReset').css('z-index', '0');
@@ -34,16 +47,25 @@ myApp.handlePlayAgainClick = function () {
   $('.winDeclaration').html(' ');
   $('.numberOption').attr('disabled', false);
 };
+// fade in header text on pageload
+myApp.headerAnimate = function () {
+  $('.hidden').fadeIn(1250).removeClass('hidden');
+};
+// play again click function
 myApp.playAgain = function () {
   $('.playAgain').on('click', myApp.handlePlayAgainClick);
 };
+// number option click function
 myApp.clickNumbers = function () {
   $('.numberOption').on('click', myApp.handleChoiceClick);
 };
+// initialization
 myApp.init = function () {
   myApp.clickNumbers();
   myApp.playAgain();
+  myApp.headerAnimate();
 };
+// pageload
 $(function () {
   myApp.init();
 });
