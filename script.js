4d756f139bd46255e9db577cb99fce8be8686e97
David <daveratcliffe93@gmail.com>
2020-11-15 22:42:25 -0500

    styled and media queries added

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ myApp.handleChoiceClick = function () {
   $('.numberOption').attr('disabled', true);
   // save randomly generate number between zero and five for computer player
   const opponentNumber = Math.floor(Math.random() * 5) + 1;
-  console.log(opponentNumber);
   // have the chosen number for the user and the computer display on the page with a hand
   const displayHand = function () {
     $(`.evenFinger${userNumber}`).css('z-index', '10');
@@ -20,25 +19,26 @@ myApp.handleChoiceClick = function () {
   const summedFingers = userNumber + opponentNumber;
   if (summedFingers % 2 === 0) {
     $('.winSum').html(`${summedFingers} fingers`);
-    $('.winDeclaration').html('evens wins');
+    $('.winDeclaration').html('even wins').css('color', '#6E954B');
     $(`.playAgain`).css('visibility', 'visible');
     // add score to user
-    let userScore = 0;
-    userScore += 1;
-    $('.evenScore').html(userScore);
-    console.log(userScore);
+    myApp.userScore++;
+    $('.evenScore').html(myApp.userScore);
   } else {
     $('.winSum').html(`${summedFingers} fingers`);
-    $('.winDeclaration').html('odds wins');
+    $('.winDeclaration').html('odd wins').css('color', '#B34248');
     $(`.playAgain`).css('visibility', 'visible');
     // add score to opponent
-    let opponentScore = 0;
-    opponentScore += 1;
-    $('.oddScore').html(opponentScore);
-    console.log(opponentScore);
+    myApp.opponentScore++;
+    $('.oddScore').html(myApp.opponentScore);
   }
 };
-
+myApp.resetScores = function () {
+  myApp.userScore = 0;
+  $('.evenScore').html(myApp.userScore);
+  myApp.opponentScore = 0;
+  $('.oddScore').html(myApp.opponentScore);
+};
 // create a play again button the resets the code
 myApp.handlePlayAgainClick = function () {
   $('.handReset').css('z-index', '0');
@@ -61,9 +61,12 @@ myApp.clickNumbers = function () {
 };
 // initialization
 myApp.init = function () {
+  myApp.userScore = 0;
+  myApp.opponentScore = 0;
   myApp.clickNumbers();
   myApp.playAgain();
   myApp.headerAnimate();
+  myApp.resetScores();
 };
 // pageload
 $(function () {
